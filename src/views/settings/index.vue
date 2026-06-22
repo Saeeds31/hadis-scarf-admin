d08bf8eadaf1c823e1a867ad958ad56ef343c498
saeed dehghan <saeeddehghans31@gmail.com>
2026-06-22 12:02:46 +0330

    :

--- src/views/settings/index.vue
+++ src/views/settings/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container mt-4" v-if="checkPermission(['setting_view', 'setting_store'])">
+    <div class="container mt-4">
         <!-- انتخاب گروه -->
         <div class="mb-3">
             <label class="form-label">انتخاب گروه</label>
@@ -14,7 +14,7 @@
         <form v-else @submit.prevent="saveSettings">
             <div v-if="settings.length > 0" class="card p-3">
                 <div v-for="(setting, index) in settings" :key="setting.id" class="mb-3">
-                    <label class="form-label fw-bold">{{ setting.label }}</label>
+                    <label class="form-label fw-bold">{{ setting.key_label }}</label>
                     <!-- input -->
                     <input v-if="setting.type === 'string'" type="text" class="form-control" v-model="setting.value" />
                     <!-- textarea -->
@@ -27,9 +27,13 @@
                         v-model="setting.value" />
                     <!-- boolean -->
                     <div v-else-if="setting.type === 'boolean'" class="form-check">
-                        <input type="checkbox" class="form-check-input" v-model="setting.value" />
+                        <input type="checkbox" class="form-check-input" v-momaindel="setting.value" />
                         <label class="form-check-label">فعال</label>
                     </div>
+                    <b-col v-else-if="setting.type === 'file'" cols="12" md="12">
+                        <VueFileAgent @select="fileloaded($event, setting.key)" ref="kos" :maxFiles="1"
+                            accept=".pdf,.jpg,.png,.webp" theme="grid" deletable sortable />
+                    </b-col>
 
                     <!-- پیشفرض -->
                     <input v-else type="text" class="form-control" v-model="setting.value" />
@@ -37,7 +41,7 @@
 
                 <!-- دکمه ذخیره -->
                 <div class="text-end">
-                    <button class="btn btn-primary" type="submit">
+                    <button class="btn btn-primary" :disabled="loader" type="submit">
                         ذخیره تنظیمات
                     </button>
                 </div>
@@ -60,7 +64,6 @@ import editor from "@/components/shared/editor.vue";
 
 import { toast } from 'vue3-toastify'
 import 'vue3-toastify/dist/index.css'
-
 import { useAdmin } from '@/stores/modules/admin';
 const store = useAdmin();
 const checkPermission = store.checkPermission;
@@ -71,6 +74,18 @@ const loading = ref(false);
 watch(() => selectedGroup.value, () => {
     fetchSettings();
 });
+function changeFile(e) {
+
+
+}
+function fileloaded(files, key) {
+    if (key && files[0] && files[0].file) {
+        let findedIndex = settings.value.findIndex((set) => set.key == key)
+        if (findedIndex) {
+            settings.value[findedIndex].value = files[0].file
+        }
+    }
+}
 const fetchGroups = async () => {
     try {
         const { data } = await axios.get("/settings-groups");
@@ -92,14 +107,29 @@ const fetchSettings = async () => {
         loading.value = false;
     }
 };
-
+let loader = ref(false);
 const saveSettings = async () => {
     try {
-        await axios.post("/settings-save-group/" + selectedGroup.value, { settings: settings.value });
+        loader.value = true;
+        const fd = new FormData();
+        settings.value.forEach((sett, index) => {
+            if (sett.id) {
+                fd.append(`settings[${index}][id]`, sett.id)
+            }
+            fd.append(`settings[${index}][key]`, sett.key)
+            fd.append(`settings[${index}][value]`, sett.value)
+            fd.append(`settings[${index}][type]`, sett.type)
+            fd.append(`settings[${index}][group]`, sett.group)
+        });
+
+        await axios.post("/settings-save-group/" + selectedGroup.value, fd);
         toast.success('تنظیمات با موفقیت ذخیره شد ✅')
     } catch (err) {
         console.log(err);
         toast.success('مشکلی در ذخیره پیش آمد ✅')
+    } finally {
+        loader.value = false;
+
     }
 };
 
